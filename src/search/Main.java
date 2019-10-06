8ec5f8c2363ec53dda7dc269bc75bb0466b9319d
AzartA <ershovna@mail.ru>
2019-10-06 19:48:04 +0300

    Stage 1 done
    Signed-off-by: AzartA <ershovna@mail.ru>

--- src/search/Main.java
+++ src/search/Main.java
@@ -1,10 +1,20 @@
 package search;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		String [] strs = sc.nextLine().split(" ");
+		String match = sc.next();
+		for (int i = 0; i < strs.length; i++) {
+			if(match.equals(strs[i])) {
+				System.out.println(i+1);
+				return;
+			}
+		}
+		System.out.println("Not found");
 	}
 
 }
