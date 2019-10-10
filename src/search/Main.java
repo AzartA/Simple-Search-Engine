5384eff15770d849c1cd403dfb9fc1a7b0bb04eb
AzartA <ershovna@mail.ru>
2019-10-10 12:34:44 +0300

    Stage 3.
    Signed-off-by: AzartA <ershovna@mail.ru>

--- src/search/Main.java
+++ src/search/Main.java
@@ -1,26 +1,47 @@
 package search;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number of people:");
+		
+		System.out.println("Enter a number of people:");
 		int n =sc.nextInt();
 		sc.nextLine(); // omit \n after integer
 		String[] lines = new String[n];
-		System.out.println("Enter all people:");
+		System.out.println("Enter all lines with people:");
 		for (int i = 0; i < n; i++) {
 			lines[i] = sc.nextLine();
 		}
-		System.out.println("Enter the number of search queries:");
-		int m =sc.nextInt();
-		sc.nextLine(); // omit \n after integer
-		for (int i = 0; i < m; i++) {
-			System.out.println("Enter a search querie:");
-			System.out.println("Found people: \n" + search(sc.nextLine(),lines));
+		
+		while(true) {
+			System.out.println("\r\n=== Menu ===\r\n" + 
+					"1. Find a person\r\n" + 
+					"2. Print all people\r\n" + 
+					"0. Exit\r\n Your choice: ");
+			int choice = sc.nextInt();
+			sc.nextLine();
+			switch (choice) {
+			case 0:
+				System.out.println("Bye!");
+				return;
+			case 1:
+					
+				System.out.println("Enter a name or email to search all suitable people:");
+				System.out.println("Found people: \n" + search(sc.nextLine(),lines));
+				break;
+			case 2:
+				System.out.println("=== List of people ===");
+				for (String ln : lines) {
+					System.out.println(ln);
+				}
+				break;
+			default:
+				System.out.println("Incorrect option! Try again.");
+				break;
+			}
 		}
 		
 	}
